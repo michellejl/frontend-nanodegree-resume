7fa4924e00deb0af8211a26c21dae8bed88add49
Michelle JL <michelle@michellejl.com>
2017-08-02 01:02:47 -0700

    finished main sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -30,6 +30,7 @@ var bio = {
     if (bio.contacts.blog) { contactsArray.push([HTMLblog, bio.contacts.blog]) }
 
     appendInfo('#topContacts', contactsArray)
+    appendInfo('#footerContacts', contactsArray)
     // TODO: use HTMLcontactGeneric from helper.js
     // to add other contact information if provided.
 
@@ -44,7 +45,6 @@ var bio = {
     })
   }
 }
-
 
 var education = {
   school: [
@@ -138,9 +138,11 @@ var education = {
       }
   ],
   display: function() {
+
+    // Formats and Displays Schools
     var schoolEducation = education.school
     var formattedSchoolEducation = schoolEducation.map(function(school) {
-      // Handles the possibility of multipl majors
+      // Handles the possibility of multiple majors
       if (school.majors.length > 1) {
         var majors = school.majors.join(', ')
       } else {
@@ -155,30 +157,147 @@ var education = {
       return formattedSchool
     })
     formattedSchoolEducation.forEach(function(school) {
-      $("#education").append($(HTMLschoolStart).append(school))
+      $('#education').append($(HTMLschoolStart).append(school))
+    })
+
+    // Formats and Displays Online Courses
+    var onlineEducation = education.onlineCourses
+    var formattedOnlineEducation = onlineEducation.map(function(course) {
+      formattedCourse = HTMLonlineTitle.replace("%data%", course.title).replace("#", course.url)
+                      + HTMLonlineSchool.replace("%data%", course.school)
+                      + HTMLonlineDates.replace("%data%", course.dates)
+                      + HTMLonlineURL.replace("%data%", course.url)
+                      + "<hr />"
+      return formattedCourse
+    })
+    $('#education').append(HTMLonlineClasses)
+    formattedOnlineEducation.forEach(function(course) {
+      $('#education').append($(HTMLschoolStart).append(course))
     })
   }
 }
 
+var work = {
+  jobs: [
+    {
+      employer: "Stackery",
+      title: "Contract Software Engineer at a startup working in the serverless space",
+      location: "Portland, Oregon",
+      dates: "March 2017 - July 2017",
+      description: "Contract role where I built and designed features from wireframes all the way through to creating the backend API. In this position I worked with: HTML, CSS, Sass, JavaScript, jQuery, jQuery UI, Node.js, Hapi.js, Joi.js, Git, GitHub, D3.js, Handlebars, API Design, MySQL, SVG Graphics, Canvas, Graphic Design, UX Design, Amazon Lambda, Amazon AWS, Amazon SES, and Amazon Cognito."
+    },
+    {
+      employer: "Girl Friend Factor",
+      title: "Freelance Web Developer and Graphic Designer for a Non-Profit",
+      location: "Remote",
+      dates: "September 2016 – Present",
+      description: "Recreated website from scratch using WordPress. In this position I worked with: HTML, CSS, WordPress, PHP, Design, Maintenance, and Training."
+    },
+    {
+      employer: "Biamp",
+      title: "Contract Front End Web Developer Working on Enterprise Software",
+      location: "Portland, Oregon",
+      dates: "August 2016 - January 2016",
+      description: "Converted non-web-based software to new web based interface. In this position I worked with: HTML, CSS, Sass, JavaScript, jQuery, Backbone.js, Underscore.js, Gulp.js, and Scrum."
+    },
+    {
+      employer: "Treehouse",
+      title: "Mentor & Code Reviewer for Front End Web Development Tech Degree Program",
+      location: "Remote / Portland, Oregon",
+      dates: "April 2016 - September 2016",
+      description: "Collaborated with students in one-on-one video sessions to help them learn how to solve problems and debug their code. In this position I worked with: HTML, CSS, Sass, JavaScript, jQuery, APIs, Responsive Design, Accessibility, SVG Graphics, HTML5 Video, Pair Programming, and Mentoring."
+    },
+    {
+      employer: "Tech Academy",
+      title: "Instructor for Full Stack Development Bootcamp",
+      location: "Portland, Oregon",
+      dates: "June 2015 - January 2016",
+      description: "Worked directly with local and remote students to ensure the mastery of languages across the stack as part of a software development bootcamp. In this position I worked with: HTML, CSS, JavaScript, jQuery, PHP, SQL, Python, Version Control, Git, GitHub, Visual Studio, Scrum, Agile, Project Management, and Mentoring."
+    },
+    {
+      employer: "Graphtek Interactive",
+      title: "Junior Developer for Creative Web Agency",
+      location: "Palm Desert, California",
+      dates: "January 2014 - March 2015",
+      description: "Worked within a legacy system to perform updates to both the content and the structure of various client websites. In this position I worked with: HTML, CSS, JavaScript, jQuery, Subversion, Adobe Photoshop, Adobe Illustrator, Adobe InDesign, Social Media Management, and SEO."
+    }
 
-// var onlineEducation = education.online
-// var formattedOnlineEducation = onlineEducation.map(function(course) {
-//   formattedCourse = HTMLonlineTitle.replace("%data%", course.title)
-//                   + HTMLonlineSchool.replace("%data%", course.school)
-//                   + HTMLonlineDates.replace("%data%", course.dates)
-//                   + HTMLonlineURL.replace("%data%", course.url)
-//   return formattedCourse
-// })
-//
-//
-//
-// $("#education").append(HTMLonlineClasses)
-//
-// formattedOnlineEducation.forEach(function(course) {
-//   $("#education").append($(HTMLschoolStart).append(course))
-// })
+  ],
+  display: function() {
+    var workExperience = work.jobs
+    workExperience.forEach(function(position) {
+      var formattedPosition = HTMLworkEmployer.replace("%data%", position.employer)
+                            + HTMLworkTitle.replace("%data%", position.title)
+                            + HTMLworkDates.replace("%data%", position.dates)
+                            + HTMLworkLocation.replace("%data%", position.location)
+                            + HTMLworkDescription.replace("%data%", position.description)
+      $("#workExperience").append($(HTMLworkStart).append(formattedPosition))
+    })
+  }
+}
 
+var projects = {
+  projects: [
+    {
+      title: "Portfolio",
+      dates: "July 2017",
+      description: "Portfolio created as part of the Udacity Front End Web Developer Nanodegree",
+      images: ["images/udacity-portfolio.png"],
+      url: "https://michellejl.github.io/portfolio-project/"
+    },
+    {
+      title: "Animal Card",
+      dates: "July 2017",
+      description: "Animal Card project for Udacity Front End Web Developer Nanodegree",
+      images: ["images/animal-card-udacity.png"],
+      url: "https://michellejl.github.io/animal-card/"
+    },
+    {
+      title: "Main Portfolio Site",
+      dates: "June 2017",
+      description: "Main portfolio site built using Node.js and Express.js. Pug is used for templating and Scss for styling",
+      images: ["images/portfolio-small.png", "images/portfolio-blog.png", "images/portfolio-coding-stats.png", "images/portfolio-contact.png"],
+      url: "http://michellejl.com"
+    },
+    {
+      title: "Simple JavaScript Game",
+      dates: "2015",
+      description: "Simple game created using JavaScript on Canvas",
+      images: ["images/simple-game-small.png"],
+      url: "https://codepen.io/michellejl/full/NxVOOw/"
+    },
+    {
+      title: "Girl Friend Factor",
+      dates: "2017",
+      description: "Website created for a non-profit on WordPress",
+      images: ["images/gff-small.png"],
+      url: "http://girlfriendfactor.org/"
+    },
+    {
+      title: "The World According to Ally: Travel Blog",
+      dates: "2016 / 2017",
+      description: "A travel blog created for someone who was about to spend a year abroad. Built on WordPress.",
+      images: ["images/twa2ally-small.png", "images/twa2ally-maps.png"],
+      url: "http://www.theworldaccordingtoally.com/"
+    }
+  ],
+  display: function() {
+    var allProjects = projects.projects
+    allProjects.forEach(function(project) {
+      var images = ""
+      for (var i = 0; i < project.images.length; i++) {
+        images = images + HTMLprojectImage.replace("%data%", project.images[i])
+      }
+      var formattedProject  = HTMLprojectTitle.replace("%data%", project.title).replace("#", project.url)
+                            + HTMLprojectDates.replace("%data%", project.dates)
+                            + HTMLprojectDescription.replace("%data%", project.description)
+                            + images
 
+      $('#projects').append($(HTMLprojectStart).append(formattedProject))
+    })
+
+  }
+}
 
 
 // dataArray should be an array or a multidimensional array containing the
@@ -202,114 +321,12 @@ function prependInfo(element, dataArray) {
     }
   }
 }
-// TODO: combine prependInfo() and appendInfo() into formatAndDisplay()
-// function formatAndDisplay() {}
-
 
 function displayAll() {
-  bio.display();
-  education.display();
+  bio.display()
+  education.display()
+  work.display()
+  projects.display()
 }
 
-displayAll();
-
-
-var work = {
-  jobs: [{
-    "employer": "Stackery",
-    "title": "Contract Software Engineer at a startup working in the serverless space",
-    "dates": "March 2017 - July 2017",
-    "location": "Portland, Oregon",
-    "description": "Contract role where I built and designed features from wireframes all the way through to creating the backend API."
-  },
-  {
-    "employer": "Girl Friend Factor",
-    "title": "Freelance Web Developer and Graphic Designer for a Non-Profit",
-    "dates": "September 2016 – Present",
-    "location": "Remote",
-    "description": "Recreated website from scratch using WordPress."
-  },
-  {
-    "employer": "Biamp",
-    "title": "Contract Front End Web Developer Working on Enterprise Software",
-    "dates": "August 2016 - January 2016",
-    "location": "Portland, Oregon",
-    "description": "Converted non-web-based software to new web based interface"
-  },
-  {
-    "employer": "Treehouse",
-    "title": "Mentor & Code Reviewer for Front End Web Development Tech Degree Program",
-    "dates": "April 2016 - September 2016",
-    "location": "Remote",
-    "description": "Collaborated with students in one-on-one video sessions to help them learn how to solve problems and debug their code."
-  },
-  {
-    "employer": "Tech Academy",
-    "title": "Instructor for Full Stack Development Bootcamp",
-    "dates": "June 2015 - January 2016",
-    "location": "Portland, Oregon",
-    "description": "Worked directly with local and remote students to ensure the mastery of languages across the stack as part of a software development bootcamp."
-  },
-  {
-    "employer": "Graphtek Interactive",
-    "title": "Junior Developer for Creative Web Agency",
-    "dates": "January 2014 - March 2015",
-    "location": "Palm Desert, California",
-    "description": "Worked within a legacy system to perform updates to both the content and the structure of various client websites."
-  }]
-}
-
-
-function displayBio() {}
-
-function displayWork() {
-  var workExperience = work.jobs
-  workExperience.forEach(function(position) {
-    var formattedPosition = HTMLworkEmployer.replace("%data%", position.employer)
-                          + HTMLworkTitle.replace("%data%", position.title)
-                          + HTMLworkDates.replace("%data%", position.dates)
-                          + HTMLworkLocation.replace("%data%", position.location)
-                          + HTMLworkDescription.replace("%data%", position.description)
-    $("#workExperience").append($(HTMLworkStart).append(formattedPosition))
-  })
-}
-
-// displayWork()
-
-$(document).click(function(loc) {
-  var x = loc.pageX
-  var y = loc.pageY
-  logClicks(x, y)
-});
-
-
-/*
-
-
-// var workExperience = work.jobs
-// var formattedWork = workExperience.map(function(position) {
-//   formattedPosition = HTMLworkEmployer.replace("%data%", position.employer)
-//                     + HTMLworkTitle.replace("%data%", position.title)
-//                     + HTMLworkDates.replace("%data%", position.dates)
-//                     + HTMLworkLocation.replace("%data%", position.location)
-//                     + HTMLworkDescription.replace("%data%", position.description)
-//   return formattedPosition
-// })
-
-
-
-// $("#header").prepend(formattedRole)
-// $("#header").prepend(formattedName)
-// $("#topContacts").append(formattedGithub)
-// $("#topContacts").append(formattedTwitter)
-// $("#topContacts").append(formattedBlog)
-// $("#topContacts").append(formattedLocation)
-
-
-
-// formattedWork.forEach(function(position) {
-//   $("#workExperience").append($(HTMLworkStart).append(position))
-// })
-
-
-*/
+displayAll()
